b89db34d16fd4c151cac55e2771dba751c02dde5
thomps0189 <angelathompson2011@gmail.com>
2022-01-03 12:02:13 -0600

    api routes

---
+++ db/connection.js
@@ -0,0 +1,12 @@
+const mysql = require('mysql2');
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  // Your MySQL username,
+  user: 'root',
+  // Your MySQL password
+  password: 'Bella!1dog',
+  database: 'election'
+});
+
+module.exports = db;
